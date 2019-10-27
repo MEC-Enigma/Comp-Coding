8a8e36e2d24459b59cb568e504bfc675bb4cdf7c
Ananta Srikar <AnantaSrikar@github.com>
2019-10-27 16:45:45 +0530

    Replaced spaces with tabs

--- Problems/Problem_2/problem_2.py
+++ Problems/Problem_2/problem_2.py
@@ -6,20 +6,20 @@ Created on Sat Oct 26 18:53:36 2019
 """
 
 def f(n):
-    
-    if n == 1 or n == 2: 
-        """Assuming the first two values are same as that of fibonacci"""
-        return n-1
-    else:
-        return (f(n-1)**(n-1) + f(n-2)**(n-2))
-    
+	
+	if n == 1 or n == 2: 
+		"""Assuming the first two values are same as that of fibonacci"""
+		return n-1
+	else:
+		return (f(n-1)**(n-1) + f(n-2)**(n-2))
+	
 def main():
-    
-    print('f(n) = f(n-1)^(n-1) + f(n-2)^(n-2)')
-    n = int(input('Enter Nth term : '))
-    val = f(n)
-    print('f(' + str(n) + ') = ' + str(val))
-    
+	
+	print('f(n) = f(n-1)^(n-1) + f(n-2)^(n-2)')
+	n = int(input('Enter Nth term : '))
+	val = f(n)
+	print('f(' + str(n) + ') = ' + str(val))
+	
 if __name__ == '__main__':
-    main()
-    
+	main()
+	

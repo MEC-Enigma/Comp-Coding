b9503ac8d2955fa4370f81cc56f7135349d10d87
Ananta Srikar <AnantaSrikar@github.com>
2019-10-27 16:52:54 +0530

    Replaced spaces with tabs

--- Problems/Problem_4/problem_4.py
+++ Problems/Problem_4/problem_4.py
@@ -6,25 +6,25 @@ Created on Sat Oct 26 20:56:42 2019
 """
 
 def fact(n):
-    fact = 1
-    while(n > 0):
-        fact *= n
-        n -= 1
-    return fact
+	fact = 1
+	while(n > 0):
+		fact *= n
+		n -= 1
+	return fact
 
 def sumOfDigits(n):
-    SUM = 0
-    while(n>0):
-        digit = n % 10
-        SUM += digit
-        n = n // 10
-    return SUM
-        
+	SUM = 0
+	while(n>0):
+		digit = n % 10
+		SUM += digit
+		n = n // 10
+	return SUM
+		
 def main():
-    
-    n = 78  #You can also input a number here
-    print(str(n) + '! = ' + str(fact(n)))
-    print('Sum of Digits = ' + str(sumOfDigits(fact(n))))
+	
+	n = 78  #You can also input a number here
+	print(str(n) + '! = ' + str(fact(n)))
+	print('Sum of Digits = ' + str(sumOfDigits(fact(n))))
 
 if __name__ == '__main__':
-    main()      
+	main()	  
